fb5275906764c1377e254daa1605d593cf3c77dd
Kalvine <kkalvine4@gmail.com>
2024-07-08 00:50:26 +0300

    Adds Event Listeners to Form

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+document.addEventListener(`DOMContentLoaded`, ()=>{
+  const form = document.querySelector(`form`);
+  
+  form.addEventListener(`submit`, (e)=>{
+    e.preventDefault();
+    addsTasks();
+    form.reset();
+  })
+
+  const tasks = document.querySelector(`#tasks`); 
+  tasks.addEventListener(`click`, ()=> deleteTask());
+ 
 });
+
+function addsTasks(){
+  const todo = document.querySelector(`#new-task-description`).value;
+  const li = document.createElement(`li`);
+  li.textContent = `${todo}  `;
+  tasks.appendChild(li);
+
+  const btn = document.createElement('button');
+  btn.addEventListener(`click`, deleteTask)
+  btn.textContent = `x`;
+  li.appendChild(btn)
+}
+
+function deleteTask(e){
+  e.target.parentNode.remove()
+}
